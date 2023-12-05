495f9af35c06c4624103ef827a52464f66b2aa84
Zihadul Islam <zihadislam2429@gmail.com>
2023-12-05 15:18:55 +0600

    bugs resolved

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -5,11 +5,25 @@ const findLastStudentId = async () => {
   const lastStudent = await User.findOne({role: "student"}, {id: 1, _id: -1})
     .sort({createdAt: -1})
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentSemesterYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
